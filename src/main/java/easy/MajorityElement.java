85a505dd09e1432029bee6fbdacf5ba80e45082c
badrjouad <jouad.badreddine@gmail.com>
2025-05-24 23:52:43 +0200

    feat(neetcode /easy problems ): Majority Element problem solved   ) )

---
+++ src/main/java/easy/MajorityElement.java
@@ -0,0 +1,30 @@
+package easy;
+
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+
+        int count=0;
+        Map<Integer,Integer> map=new HashMap<Integer, Integer>();
+
+        for(int n : nums)
+        {
+            map.put(n,map.getOrDefault(n,0)+1);
+            if(map.get(n)> nums.length/2)
+            {
+                return n;
+            }
+        }
+
+
+
+
+        return -1;
+
+    }
+}
