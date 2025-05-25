94b096a991efbaaa194815ffdeae054fdeb5b86a
badrjouad <jouad.badreddine@gmail.com>
2025-05-25 16:33:24 +0200

    feat(neetcode /easy problems ):Max Ascending Sub Array   ) )

---
+++ src/main/java/easy/MaximumAscendingSubarray.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class MaximumAscendingSubarray {
+    public int maxAscendingSum(int[] nums) {
+
+        int maxSum=nums[0];
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                sum=sum+nums[i];
+            }
+            else{
+                sum=nums[i];
+            }
+            maxSum=Math.max(maxSum,sum);
+        }
+
+
+        return maxSum;
+    }
+}
